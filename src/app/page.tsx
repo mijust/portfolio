38cccb91da4405716639d702b7cdaf8870badfeb
Michael <mi.justus@icloud.com>
2025-02-10 08:52:17 +0100

    v2.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Header from "../components/Header"
-import Footer from "../components/Footer"
-import Hero from "../components/Hero"
-import About from "../components/About"
-import Projects from "../components/Projects"
-import Contact from "../components/Contact"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+import Hero from "@/components/Hero"
+import About from "@/components/About"
+import Projects from "@/components/Projects"
+import Contact from "@/components/Contact"
 
 export default function Home() {
   return (
